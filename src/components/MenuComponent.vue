9f248082572515b8363e72d62bf4ce353f4af11b
Aljayz <152593684+Aljayz@users.noreply.github.com>
2025-03-19 06:44:25 +0800

    Added notifcation badge if there is notification

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -1,6 +1,10 @@
 <template>
   <q-item :to="link" clickable>
-    <q-icon size="30px" :name="icon"/>
+    <q-icon size="30px" :name="icon">
+        <q-badge v-if="icon == 'favorite'" color="red" text-color="white" floating rounded/>
+      <q-tooltip :delay="1000">{{ title }}</q-tooltip>
+    </q-icon>
+
     <q-item-section v-if="$q.screen.gt.sm">
       <q-item-label class="q-pa-md q-gutter-sm">{{ title }}</q-item-label>
     </q-item-section>
